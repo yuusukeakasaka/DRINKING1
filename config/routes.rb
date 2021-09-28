b4fad7dd153ddfe02c134605367e2b616348c6a7
yuusukeakasaka <ayazato2206a@gmail.com>
2021-09-28 22:10:03 +0000

    [start]userの新規登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'site' do
+  devise_for :users, controllers: {
+    registrations: 'public/users/registrations',
+    sessions: 'public/users/sessions',
+    passwords: 'public/users/passwords'
+  }
+  end
 end
