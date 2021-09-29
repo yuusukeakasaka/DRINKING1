86546ceff0b683fcb8811254803f096f29b0606a
yuusukeakasaka <ayazato2206a@gmail.com>
2021-09-29 12:50:46 +0000

    [start]application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    if current_user
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      root_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope module: 'site' do
+
+  scope module: 'public' do
+  root to: 'homes#top'
   devise_for :users, controllers: {
     registrations: 'public/users/registrations',
     sessions: 'public/users/sessions',
