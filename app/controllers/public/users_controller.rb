6373a13fbe7ab7be62daf14915caf724cc0a211d
yuusukeakasaka <ayazato2206a@gmail.com>
2021-09-30 21:53:26 +0000

    [Add]ユーザー編集画面

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,4 +8,17 @@ class Public::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :icon_image, :introduction)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+
+
 end
