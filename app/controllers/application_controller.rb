06e4ddbf5846ba26dc57c7887970611da496a3e1
yuusukeakasaka <ayazato2206a@gmail.com>
2021-10-06 11:25:22 +0000

    [Add]管理者機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,17 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if current_user
       root_path
+    else current_admins
+      admins_root_path
     end
   end
 
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :user
       root_path
+    elsif
+      resource_or_scope == :admin
+      new_admin_session_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  namespace :admins do
+    root :to => "homes#top"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope module: 'public' do
   get "/users/:id/unsubscribe" => 'users#unsubscribe', as: 'user_unsubscribe'
