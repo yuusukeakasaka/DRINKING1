595d53750e3ac5d39bb98ef81b881d0906ccbd05
yuusukeakasaka <ayazato2206a@gmail.com>
2021-10-06 08:03:41 +0000

    [Update]募集機能

--- app/controllers/public/recruitments_controller.rb
+++ app/controllers/public/recruitments_controller.rb
@@ -1,2 +1,30 @@
 class Public::RecruitmentsController < ApplicationController
+
+  def new
+    @recruitment = Recruitment.new
+  end
+
+  def create
+    @recruitment = Recruitment.new(recruitment_params)
+    @recruitment.user_id = current_user.id
+    @recruitment.save
+    redirect_to recruitments_path
+  end
+
+  def index
+    @recruitments = Recruitment.all
+  end
+
+  def destroy
+    @recruitment = Recruitment.find(params[:id])
+    @recruitment.destroy
+    redirect_to recruitments_path
+  end
+
+  private
+
+  def recruitment_params
+    params.require(:recruitment).permit(:recruitment_post, :user_id)
+  end
+
 end
