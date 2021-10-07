b15936a6f25fb7eb74c428d0ed7893ac7309fc71
yuusukeakasaka <ayazato2206a@gmail.com>
2021-10-07 22:53:37 +0000

    [Add]管理者の会員詳細

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -4,4 +4,12 @@ class Admins::UsersController < ApplicationController
     @users = User.all
   end
 
+  def update
+    redirect_to admins_users
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :admins do
     root :to => "homes#top"
-    resources :users, only:[:index, :edit, :update]
+    resources :users, only:[:index, :edit, :update, :show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
