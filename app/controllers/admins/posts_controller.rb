7e3987c666bc60bafef6e57a7f6a20066a5717b2
yuusukeakasaka <ayazato2206a@gmail.com>
2021-10-08 02:17:59 +0000

    [Add]管理者の投稿一覧画面

--- app/controllers/admins/posts_controller.rb
+++ app/controllers/admins/posts_controller.rb
@@ -4,4 +4,9 @@ class Admins::PostsController < ApplicationController
     @posts = Post.all.reverse_order
   end
 
+  def destroy
+    Post.find_by(id: params[:id]).destroy
+    redirect_to admins_posts_path
+  end
+
 end
