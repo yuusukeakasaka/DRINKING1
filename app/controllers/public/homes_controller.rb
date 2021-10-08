98f517bb2394f482e8c6e6c622a00c0f120c9ffb
yuusukeakasaka <ayazato2206a@gmail.com>
2021-10-08 07:09:36 +0000

    [Update]public/top.html.erb

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,16 @@
 class Public::HomesController < ApplicationController
 
   def top
+    @posts = Post.all.reverse_order
+    @post_comment = PostComment.new
   end
 
   def about
+  end
+
+  def search
+    @posts = Post.search(params[:q])
+    render "top"
   end
 
 end
